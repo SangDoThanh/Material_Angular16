bf3ebd5f9378968ea9f8297e35cbe41ca61cc503
DOTHANHSANG <dothanhsang95@gmail.com>
2023-07-05 14:29:30 +0700

    update add code tabs

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { AsyncSubject, Observable, Observer } from 'rxjs';
+import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+
+export interface ExampleTab {
+  label: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css'],
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatIconModule, NgIf, NgFor, AsyncPipe],
 })
 export class TabsComponent {
+  asyncTabs: Observable<ExampleTab[]>;
 
+  constructor() {
+    this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
+      setTimeout(() => {
+        observer.next([
+          {label: 'First', content: 'Content 1'},
+          {label: 'Second', content: 'Content 2'},
+          {label: 'Third', content: 'Content 3'},
+        ]);
+      }, 1000);
+    });
+  }
 }
